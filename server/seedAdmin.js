b178bddb3157809f3e8ada97b7fa049c1ab1e6ec
anik-aryan <anikaryan476@gmail.com>
2026-07-25 17:12:29 +0530

    Adding User

--- server/seedAdmin.js
+++ server/seedAdmin.js
@@ -1,35 +1,35 @@
-// require("dotenv").config();
+require("dotenv").config();
 
-// const mongoose = require("mongoose");
-// const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-// const Admin = require("./src/models/admin.model");
+const Admin = require("./src/models/admin.model");
 
-// async function seedAdmin() {
-//   await mongoose.connect(process.env.MONGODB_URI);
+async function seedAdmin() {
+  await mongoose.connect(process.env.MONGODB_URI);
 
-//   const exists = await Admin.findOne({
-//     email: "admin@leaddesk.com",
-//   });
+  const exists = await Admin.findOne({
+    email: "admin@leaddesk.com",
+  });
 
-//   if (exists) {
-//     process.exit();
-//   }
+  if (exists) {
+    process.exit();
+  }
 
-//   const hashedPassword = await bcrypt.hash(
-//     "Admin@123",
-//     10
-//   );
+  const hashedPassword = await bcrypt.hash(
+    "Admin@123",
+    10
+  );
 
-//   await Admin.create({
-//     name: "Admin",
-//     email: "admin@leaddesk.com",
-//     password: hashedPassword,
-//   });
+  await Admin.create({
+    name: "Admin",
+    email: "admin@leaddesk.com",
+    password: hashedPassword,
+  });
 
-//   console.log("Admin Created");
+  console.log("Admin Created");
 
-//   process.exit();
-// }
+  process.exit();
+}
 
-// seedAdmin();
+seedAdmin();
