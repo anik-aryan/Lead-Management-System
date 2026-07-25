31ebca1c78fe56905247cda05fa75cb8982ee5b8
anik-aryan <anikaryan476@gmail.com>
2026-07-25 18:11:20 +0530

    Logout fixing

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -60,7 +60,13 @@ const getMe = async (req, res) => {
 };
 
 const logout = async (req, res) => {
-  res.clearCookie("token");
+  const isProd = process.env.NODE_ENV === "production";
+
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: isProd,
+    sameSite: isProd ? "none" : "lax",
+  });
 
   res.status(200).json({
     success: true,
